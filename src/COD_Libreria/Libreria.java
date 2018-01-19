328959cff68bec816acc8e311f4b4d2583241752
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2018-01-19 09:16:30 +0100

    Mi libreria - funcion pregunta(String mensage)

--- src/COD_Libreria/Libreria.java
+++ src/COD_Libreria/Libreria.java
@@ -1,5 +1,13 @@
 package COD_Libreria;
 
+import java.awt.Component;
+import javax.swing.JOptionPane;
+
 public class Libreria {
     
+    public static int pregunta(String mensaje){
+        Component frame = null;
+        int opcion = JOptionPane.showConfirmDialog(frame, mensaje, "Pregunta", JOptionPane.YES_NO_OPTION);
+        return opcion;
+    }
 }
